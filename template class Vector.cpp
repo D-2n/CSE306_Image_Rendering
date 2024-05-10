bb3a646183676e11bac7e3bb890ae320f93ded23
D-2n <114923905+D-2n@users.noreply.github.com>
2024-05-10 04:12:27 +0200

    [Finished] All up to anti-aliasing
    Fresnel + major code restructuring for comfort

--- template class Vector.cpp
+++ template class Vector.cpp
@@ -80,41 +80,6 @@ public:
     Sphere() {
 
     };
-    /*
-    bool intersect(Ray& ray, Vector& color, Vector& P, Vector&N) const {
-        Vector u = ray.ray_unit_direction;
-        Vector O = ray.ray_origin;
-        Vector C = sphere_center;
-
-        Vector ray_color;
-        double t;
-        Vector O_minus_C = O - C;
-        double discriminant = dot(u, O_minus_C) * dot(u, O_minus_C) - O_minus_C.norm2() + sphere_radius * sphere_radius;
-        if (discriminant >= 0) {
-            double t1 = dot(u, C - O) - sqrt(discriminant);
-            double t2 = dot(u, C - O) + sqrt(discriminant);
-            if (t2 >= 0) {
-                if (t1 >= 0) {
-                    t = t1;
-                    P = O + t * u;
-                    N = (P - C) / (P - C).norm();
-                    color = sphere_albedo;
-                    return true;
-                }
-                else {
-                    t =  t2;
-                    P = O + t * u;
-                    N = (P - C) / (P - C).norm();
-                    color = sphere_albedo;
-                    return true;
-
-                }
-            }
-        }
-
-        return false;
-    };
-    */
 
     Vector sphere_center;
     double sphere_radius;
@@ -191,55 +156,52 @@ public:
     std::vector<Sphere> objects;
 
 
+    Vector random_cos() {
+        double r1 = ((double)rand() / (double)RAND_MAX); //from https://stackoverflow.com/questions/1340729/how-do-you-generate-a-random-double-uniformly-distributed-between-0-and-1-from-c
+        double r2 = ((double)rand() / (double)RAND_MAX);
+        double x = std::cos(2 * M_PI * r1) * sqrt(1 - r2);
+        double y = std::sin(2 * M_PI * r1) * sqrt(1 - r2);
+        double z = sqrt(r2);
+        return Vector(x, y, z);
+
+    }
     Vector getColor(Ray& ray, int ray_depth, Sphere& sphere, Vector& P, Vector& N, Vector& w_i) {
 
         Vector color;
         Vector color_wall;
+        Vector source(-10, 20, 40);
         if (ray_depth < 0) {
             return Vector(255., 0., 0.);
         }
         if (sphere.mirror) {
             Vector w_r = w_i - (2 * dot(w_i, N) * N);
             w_r.normalize();
-            Ray reflected_ray(P + 0.0001 * N, w_r);
+            Ray reflected_ray(P + 1e-8 * N, w_r);
             Intersection intersec = scene_intersect(reflected_ray, color, P, N);
             return getColor(reflected_ray, ray_depth - 1, intersec.S, P, N, w_r);
         }
         else {
             if (sphere.refrac) {
-                if (sphere.inside) {
-                    N = -1 * N;
-                }
-                if (dot(w_i, N) > 0) {
-                    double n1 = 1.5;
-                    double n2 = 1;
-                    //if sqrt negative, bounce ray
-                    double root = 1 - std::pow(n1 / n2, 2) * (1 - std::pow(dot(w_i, N), 2));
-                    if (root < 0) {
-                        Vector w_r = w_i - (2 * dot(w_i, N) * N);
-                        w_r.normalize();
-                        Ray reflected_ray(P + 0.0001 * N, w_r);
-                        Intersection intersec = scene_intersect(reflected_ray, color, P, N);
-                        return getColor(reflected_ray, ray_depth - 1, intersec.S, P, N, w_r);
-                    }
-                    else {
-                        N = -1 * N;
-                        Vector w_t_T = (n1 / n2) * (w_i - (dot(w_i, N) * N));
-                        Vector w_t_N = -1 * N * sqrt(root);
-                        Vector w_t = w_t_T + w_t_N;
-                        //w_t.normalize();
-
-                        Ray ray_ref(P - 0.001 * N, w_t);
-
-                        Intersection intersec = scene_intersect(ray_ref, color, P, N);
-                        return getColor(ray_ref, ray_depth - 1, intersec.S, P, N, w_t);
-                    }
+                N = (sphere.inside) ? -1 * N : N;
+                double n1, n2;
+                Vector N1;
+                n1 = (dot(w_i, N) > 0) ? 1.5 : 1;
+                n2 = (dot(w_i, N) > 0) ? 1 : 1.5;
+                N1 = (dot(w_i, N) < 0) ? N : -1 * N;
+                //if sqrt negative, bounce ray
+                double root = 1 - std::pow(n1 / n2, 2) * (1 - std::pow(dot(w_i, N), 2));
+                if (root < 0) {
+                    Vector w_r = w_i - (2 * dot(w_i, N) * N);
+                    w_r.normalize();
+                    Ray reflected_ray(P + 0.0001 * N, w_r);
+                    Intersection intersec = scene_intersect(reflected_ray, color, P, N);
+                    return getColor(reflected_ray, ray_depth - 1, intersec.S, P, N, w_r);
                 }
                 else {
-                    double n1 = 1;
-                    double n2 = 1.5;
-                    double root = 1 - std::pow(n1 / n2, 2) * (1 - std::pow(dot(w_i, N), 2));
-                    if (root < 0) {
+                    double k0 = (std::pow(n1 - n2, 2)) / (std::pow(n1 + n2, 2));
+                    double R = k0 + (1 - k0) * std::pow(1 - abs(dot(N, w_i)), 5);
+                    double fresnel_refrac = ((double)rand() / (double)RAND_MAX);
+                    if (fresnel_refrac < R) {
                         Vector w_r = w_i - (2 * dot(w_i, N) * N);
                         w_r.normalize();
                         Ray reflected_ray(P + 0.0001 * N, w_r);
@@ -247,33 +209,61 @@ public:
                         return getColor(reflected_ray, ray_depth - 1, intersec.S, P, N, w_r);
                     }
                     else {
+                        N = N1;
                         Vector w_t_T = (n1 / n2) * (w_i - (dot(w_i, N) * N));
                         Vector w_t_N = -1 * N * sqrt(root);
                         Vector w_t = w_t_T + w_t_N;
                         //w_t.normalize();
+
                         Ray ray_ref(P - 0.001 * N, w_t);
+
                         Intersection intersec = scene_intersect(ray_ref, color, P, N);
                         return getColor(ray_ref, ray_depth - 1, intersec.S, P, N, w_t);
                     }
                 }
+            }
 
-            }
-            else {
-                return sphere.sphere_albedo;
-            }
         }
-        return Vector(255., 0., 0.);
+        Vector final_color;
+
+        double eps = 1E-8;
+        Vector color_shadow;
+        Vector P_shadow, N_shadow;
+        double d = (source - P).norm();
+        Vector omega_i = (source - P) / d;
+        Ray shadow_ray(P + eps * N, omega_i);
+        int VpS = 1;
+
+
+        double Nw_i = std::max(0., dot(N, omega_i));
+        double intensity = 2.1e10;
+
+        Intersection intersec_shadow = scene_intersect(shadow_ray, color_shadow, P_shadow, N_shadow);
+
+        if (((P_shadow - P).norm2() <= (source - P).norm2())) {
+            VpS = 0;
+        }
+        Vector light = sphere.sphere_albedo;
+        light.normalize();
+        light =
+            intensity / (4 * M_PI * d * d) * (light / M_PI) * VpS * Nw_i;
+
+        final_color = light;
+        Vector ray_dir = random_cos();
+        Ray randomRay(P + eps * N, ray_dir);
+        Vector color_random;
+        Vector P_rand, N_rand;
+        Intersection intersec_random = scene_intersect(randomRay, color_random, P_rand, N_rand);
+        Vector mult_color = intersec_random.S.sphere_albedo;
+        // ^^ doesnt want to do get color for some reason
+
+        final_color.data[0] += sphere.sphere_albedo.data[0] * mult_color.data[0];
+        final_color.data[1] += sphere.sphere_albedo.data[1] * mult_color.data[1];
+        final_color.data[2] += sphere.sphere_albedo.data[2] * mult_color.data[2];
+        //final_color.normalize();
+        return final_color;
     };
 };
-Vector random_cos() {
-    double r1 = ((double)rand() / (double)RAND_MAX); //from https://stackoverflow.com/questions/1340729/how-do-you-generate-a-random-double-uniformly-distributed-between-0-and-1-from-c
-    double r2 = ((double)rand() / (double)RAND_MAX);
-    double x = std::cos(2 * M_PI * r1) * sqrt(1 - r2);
-    double y = std::sin(2 * M_PI * r1) * sqrt(1 - r2);
-    double z = sqrt(r2);
-    return Vector(x, y, z);
-
-}
 int main() {
     int W = 512;
     int H = 512;
@@ -288,10 +278,10 @@ int main() {
     Sphere S_2(Vector(20, 0, 0), 10, Vector(255, 67, 189), false, true);
     Sphere S_3(Vector(20, 0, 0), 9.5, Vector(255, 67, 189), false, true);
     S_3.inside = true;
-    Sphere wall_1(Vector(0, 1000, 0), 940, Vector(0, 0, 0), false, false); // red wall
-    Sphere wall_2(Vector(0, 0, 1000), 940, Vector(0, 0, 0), false, false); // green wall
+    Sphere wall_1(Vector(0, 1000, 0), 940, Vector(255, 0, 255), false, false); // red wall
+    Sphere wall_2(Vector(0, 0, 1000), 940, Vector(0, 255, 0), false, false); // green wall
     Sphere wall_5(Vector(1000, 0, 0), 940, Vector(0, 255, 255), false, false); //side left
-    Sphere wall_6(Vector(-1000, 0, 0), 940, Vector(255, 215, 171), false, false); //side right
+    Sphere wall_6(Vector(-1000, 0, 0), 940, Vector(255, 0, 0), false, false); //side right
     Sphere wall_3(Vector(0, -1000, 0), 990, Vector(0, 0, 255), false, false); // blue wall
     Sphere wall_4(Vector(0, 0, -1000), 940, Vector(255, 255, 0), false, false); // yellow wall
 
@@ -302,61 +292,36 @@ int main() {
     std::vector<unsigned char> image(W * H * 3, 0);
     for (int i = 0; i < H; i++) {
         for (int j = 0; j < W; j++) {
-            Vector color_wall;
+            Vector color_wall(0., 0., 0.);
             Vector color_shadow;
 
             Vector P, N;
-            Vector P_shadow, N_shadow;5
+            Vector P_shadow, N_shadow;
             // Camera ray initialization
             Vector pixel_coord(Q.data[0] + j + 0.5 - W / 2, Q.data[1] + H - i - 1 + 0.5 - H / 2, Q.data[2] - W / (2 * tan(alpha * M_PI / 360)));
             Vector ray_direction = pixel_coord - Q;
             ray_direction.normalize();
             Ray ray_camera(Q, ray_direction);
-            int n_rays = 10;
-            Intersection intersec_wall = main_scene.scene_intersect(ray_camera, color_wall, P, N);
-            for (int i = 0; i < n_rays; i++) {
+            int n_rays = 1000;
+            double r = 0.;
+            double g = 0.;
+            double b = 0.;
+            for (int counter = 0; counter < n_rays; counter++) {
+                Vector color_inter;
                 Intersection intersec_wall = main_scene.scene_intersect(ray_camera, color_wall, P, N);
-                Vector color_inter = main_scene.getColor(ray_camera, 5, intersec_wall.S, P, N, ray_direction);
-                color_wall.data[0] += color_inter.data[0];
-                color_wall.data[1] += color_inter.data[1];
-                color_wall.data[2] += color_inter.data[2];
+                color_inter = main_scene.getColor(ray_camera, 5, intersec_wall.S, P, N, ray_direction);
+                r += color_inter.data[0];
+                g += color_inter.data[1];
+                b += color_inter.data[2];
             }
-            color_wall.data[0] /= n_rays;
-            color_wall.data[1] /= n_rays;
-            color_wall.data[2] /= n_rays;
-            image[(i * W + j) * 3 + 0] = std::max(0., color_wall[0]);
-            image[(i * W + j) * 3 + 1] = std::max(0., color_wall[1]);
-            image[(i * W + j) * 3 + 2] = std::max(0., color_wall[2]);
-            double d = (light_source - P).norm();
-            Vector w_i = (light_source - P) / d;
-            double epsilon = 1;
-            Ray shadow_ray(P + epsilon * N, w_i);
-            int VpS = 1;
+            n_rays += 1;
+            color_wall.data[0] = r / (double)n_rays;
+            color_wall.data[1] = g / (double)n_rays;
+            color_wall.data[2] = b / (double)n_rays;
 
-            //shadowing
-            Intersection intersec_shadow = main_scene.scene_intersect(shadow_ray, color_shadow, P_shadow, N_shadow);
+            Vector current_color = color_wall;
 
-            if (((P_shadow - P).norm2() <= (light_source - P).norm2())) {
-                VpS = 0;
-            }
-
-            double Nw_i = std::max(0., dot(N, w_i));
-            double intensity = 2.1e10;
-
-            //set the albedo a.k.a the color of the pixel
-            Vector current_color(image[(i * W + j) * 3 + 0], image[(i * W + j) * 3 + 1], image[(i * W + j) * 3 + 2]);
-            Vector albedo(image[(i * W + j) * 3 + 0], image[(i * W + j) * 3 + 1], image[(i * W + j) * 3 + 2]);
-
-            current_color.normalize();
-            // run it through the formula
-            current_color = (intensity / (4 * M_PI * d * d)) * (current_color / M_PI) * VpS * Nw_i;
-            Vector ray_dir = random_cos();
-            Ray randomRay(P, ray_dir);
-            Vector mult_color = main_scene.getColor(randomRay, 5, intersec_wall.S, P, N, ray_dir);
-            current_color.data[0] += albedo.data[0] * mult_color.data[0];
-            current_color.data[1] += albedo.data[1] * mult_color.data[1];
-            current_color.data[2] += albedo.data[2] * mult_color.data[2];
-            //set pixel color to updated shadow one
+            //current_color.normalize();
             image[(i * W + j) * 3 + 0] = std::min(255., std::pow(current_color[0], 1 / 2.2));
             image[(i * W + j) * 3 + 1] = std::min(255., std::pow(current_color[1], 1 / 2.2));
             image[(i * W + j) * 3 + 2] = std::min(255., std::pow(current_color[2], 1 / 2.2));
